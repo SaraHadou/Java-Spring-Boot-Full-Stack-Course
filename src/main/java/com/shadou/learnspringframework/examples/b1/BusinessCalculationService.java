97340cda60913d89aa49320fbfacc015fb6afeee
SaraHadou <s.hadou.user@gmail.com>
2024-05-04 22:10:13 +0300

    real world spring framework example.

---
+++ src/main/java/com/shadou/learnspringframework/examples/b1/BusinessCalculationService.java
@@ -0,0 +1,22 @@
+package com.shadou.learnspringframework.examples.b1;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class BusinessCalculationService {
+
+    DataService dataService;
+
+    public BusinessCalculationService(@Qualifier("mongoDbDataService") DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    public int findMax() {
+        return Arrays.stream(dataService.retrieveData())
+                .max().orElse(0);
+    }
+
+}
---
+++ src/main/java/com/shadou/learnspringframework/examples/b1/DataService.java
@@ -0,0 +1,5 @@
+package com.shadou.learnspringframework.examples.b1;
+
+public interface DataService {
+    int[] retrieveData();
+}
