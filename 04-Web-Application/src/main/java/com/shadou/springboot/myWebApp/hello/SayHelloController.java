550ac73f4b19b531064655abbaaf0ca30df08bc7
SaraHadou <s.hadou.user@gmail.com>
2024-05-14 12:26:55 +0300

    Add sayHelloController - Basics

--- 04-Web-Application/src/main/java/com/shadou/springboot/myWebApp/hello/SayHelloController.java
+++ 04-Web-Application/src/main/java/com/shadou/springboot/myWebApp/hello/SayHelloController.java
@@ -1,4 +1,35 @@
 package com.shadou.springboot.myWebApp.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello!";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My First HTML Page </title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("my first html page with body");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
+    }
 }
