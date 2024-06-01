307a9e48b97864e854743faf6c9c6bcd641ab2f6
SaraHadou <s.hadou.user@gmail.com>
2024-06-01 13:04:25 +0300

    explore AfterThrowing & AfterReturning

--- 11-learn-spring-aop/src/main/java/com/shadou/learn_spring_aop/aop/aspects/LoggingAspect.java
+++ 11-learn-spring-aop/src/main/java/com/shadou/learn_spring_aop/aop/aspects/LoggingAspect.java
@@ -1,9 +1,7 @@
 package com.shadou.learn_spring_aop.aop.aspects;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -18,19 +16,29 @@ public class LoggingAspect {
     // execution(* PACKAGE.*.*(..))
     @Before("execution(* com.shadou.learn_spring_aop.aop.*.*.*(..))")
     public void logMethodCallBeforeExecution(JoinPoint joinPoint) {
-        // Logic - What?
         logger.info("Before aspect - {} is called with arguments: {}",
-                joinPoint, joinPoint.getArgs());
+                joinPoint, joinPoint.getArgs()); // What ?
     }
 
     @After("execution(* com.shadou.learn_spring_aop.aop.*.*.*(..))")
     public void logMethodCallAfterExecution(JoinPoint joinPoint) {
-        // Logic - What?
         logger.info("After aspect - {} has executed ", joinPoint);
     }
 
+    @AfterThrowing (
+            pointcut = "execution(* com.shadou.learn_spring_aop.aop.*.*.*(..))",
+            throwing = "exception"
+    )
+    public void logMethodCallAfterException(JoinPoint joinPoint, Exception exception) {
+        logger.info("AfterThrowing aspect - {} has thrown exception {}", joinPoint, exception);
+    }
 
-
-
+    @AfterReturning(
+            pointcut = "execution(* com.shadou.learn_spring_aop.aop.*.*.*(..))",
+            returning = "result"
+    )
+    public void logMethodCallAfterSuccessExecution(JoinPoint joinPoint, Object result) {
+        logger.info("AfterReturning aspect - {} has returned {}", joinPoint, result);
+    }
 
 }
