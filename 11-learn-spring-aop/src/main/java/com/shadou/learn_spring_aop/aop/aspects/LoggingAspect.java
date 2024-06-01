1abdbb689603cc0446f6dadc68b82b3d183df647
SaraHadou <s.hadou.user@gmail.com>
2024-06-01 11:13:52 +0300

    explore @After

--- 11-learn-spring-aop/src/main/java/com/shadou/learn_spring_aop/aop/aspects/LoggingAspect.java
+++ 11-learn-spring-aop/src/main/java/com/shadou/learn_spring_aop/aop/aspects/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.shadou.learn_spring_aop.aop.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,11 +14,19 @@ public class LoggingAspect {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    // Pointcut - When>
-    @Before("execution(* com.shadou.learn_spring_aop.aop.business.*.*(..))")
-    public void logMethodCall(JoinPoint joinPoint) {
+    // Pointcut - When?
+    // execution(* PACKAGE.*.*(..))
+    @Before("execution(* com.shadou.learn_spring_aop.aop.*.*.*(..))")
+    public void logMethodCallBeforeExecution(JoinPoint joinPoint) {
         // Logic - What?
-        logger.info("Before aspect - Method is called - {}", joinPoint);
+        logger.info("Before aspect - {} is called with arguments: {}",
+                joinPoint, joinPoint.getArgs());
+    }
+
+    @After("execution(* com.shadou.learn_spring_aop.aop.*.*.*(..))")
+    public void logMethodCallAfterExecution(JoinPoint joinPoint) {
+        // Logic - What?
+        logger.info("After aspect - {} has executed ", joinPoint);
     }
 
 
