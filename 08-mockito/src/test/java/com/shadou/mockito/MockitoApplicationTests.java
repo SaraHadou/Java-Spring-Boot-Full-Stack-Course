973586de5ea797bba8e944ec9e0739f22dedfe8f
SaraHadou <s.hadou.user@gmail.com>
2024-06-09 19:54:36 +0300

    explore mockito

---
+++ 08-mockito/src/test/java/com/shadou/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadou.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
