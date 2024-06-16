e31ee7bba77c93564d08347ce5cb6ac382629cdc
SaraHadou <s.hadou.user@gmail.com>
2024-06-17 00:31:31 +0300

    storing credentials

--- 06-spring-security/src/main/java/com/shadou/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ 06-spring-security/src/main/java/com/shadou/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -2,9 +2,18 @@ package com.shadou.learn_spring_security.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -19,15 +28,71 @@ public class BasicAuthSecurityConfiguration {
         });
 
         http.sessionManagement( session ->
-                session.sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS));
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
 
 //        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
 
-        http.csrf(csrf -> csrf.disable());
+        http.csrf( csrf -> csrf.disable());
+
+        http.headers( headers ->
+                headers.frameOptions( frameOptionsConfig ->
+                        frameOptionsConfig.disable())
+        );
 
         return http.build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailService() {
+//
+//        var user = User.withUsername("user1")
+//                .password("{noop}password")
+//                .roles("USER")
+//                .build();
+//
+//        var admin = User.withUsername("admin")
+//                .password("{noop}password")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService(DataSource dataSource) {
+
+        var user = User.withUsername("user1")
+                .password("password")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("password")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN","USER")
+                .build();
+
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
