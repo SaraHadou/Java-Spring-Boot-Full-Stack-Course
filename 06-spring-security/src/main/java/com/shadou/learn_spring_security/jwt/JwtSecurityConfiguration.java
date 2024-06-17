cc24ff2e51ff8a0485aaf1e2278eb9ffd68f9f6f
SaraHadou <s.hadou.user@gmail.com>
2024-06-17 23:34:22 +0300

    jwt

--- 06-spring-security/src/main/java/com/shadou/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ 06-spring-security/src/main/java/com/shadou/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,5 +1,10 @@
-package com.shadou.learn_spring_security.basic;
+package com.shadou.learn_spring_security.jwt;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -10,15 +15,24 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtSecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,34 +45,19 @@ public class BasicAuthSecurityConfiguration {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
 
-//        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
 
-        http.csrf( csrf -> csrf.disable());
+        http.csrf( csrf -> csrf.disable() );
 
         http.headers( headers ->
                 headers.frameOptions( frameOptionsConfig ->
-                        frameOptionsConfig.disable())
+                        frameOptionsConfig.disable() )
         );
+
+        http.oauth2ResourceServer( oauth2 -> oauth2.jwt(withDefaults()) );
 
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailService() {
-//
-//        var user = User.withUsername("user1")
-//                .password("{noop}password")
-//                .roles("USER")
-//                .build();
-//
-//        var admin = User.withUsername("admin")
-//                .password("{noop}password")
-//                .roles("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
 
     @Bean
     public DataSource dataSource() {
@@ -93,6 +92,45 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public KeyPair keyPair() {
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey
+                .Builder((RSAPublicKey)keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, context) ->  jwkSelector.select(jwkSet);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
     }
 
 }
